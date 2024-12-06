ffc7586739d58b0f3760a36863e9c61e66d52522
eddieluis37 <eddierada@gmail.com>
2024-12-06 15:53:24 -0500

    Factoriza para evitar recargar header sidebar footer en la navegacion interna y externa

--- resources/views/layouts/theme/app.blade.php
+++ resources/views/layouts/theme/app.blade.php
@@ -1,28 +1,29 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
     <title>ERP - CarnesFrias Mega</title>
-    <link rel="icon" type="image/x-icon" href="{{asset('assets/img/favicon.ico')}}"/>    
-    
+    <link rel="icon" type="image/x-icon" href="{{asset('assets/img/favicon.ico')}}" />
 
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
     @include('layouts.theme.styles')
     <!-- END PAGE LEVEL PLUGINS/CUSTOM STYLES -->
 
 </head>
+
 <body class="dashboard-analytics">
 
-    <!-- BEGIN LOADER -->
+    <!-- <!-- BEGIN LOADER
     <div id="load_screen"> <div class="loader"> <div class="loader-content">
         <div class="spinner-grow align-self-center"></div>
     </div></div></div>
-    <!--  END LOADER -->
-
+    <!--  END LOADER 
+ -->
     <!--  BEGIN NAVBAR  -->
-     @include('layouts.theme.header')
+    @include('layouts.theme.header')
     <!--  END NAVBAR  -->
 
     <!--  BEGIN MAIN CONTAINER  -->
@@ -32,18 +33,18 @@
         <div class="search-overlay"></div>
 
         <!--  BEGIN SIDEBAR  -->
-         @include('layouts.theme.sidebar')
+        @include('layouts.theme.sidebar')
         <!--  END SIDEBAR  -->
-        
+
         <!--  BEGIN CONTENT AREA  -->
         <div id="content" class="main-content">
-            
+
             <div class="layout-px-spacing">
-            
+
                 @yield('content')
 
             </div>
-            
+
 
             @include('layouts.theme.footer')
         </div>
@@ -60,4 +61,5 @@
     <!--SCRIPTS FOR EACH COMPONENT-->
     @yield('script')
 </body>
+
 </html>
