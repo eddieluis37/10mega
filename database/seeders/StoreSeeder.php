5cb6424d23667c067f9d1bfd9f5225d7bdffa78e
eddieluis37 <eddierada@gmail.com>
2024-12-30 13:16:54 -0500

    Añade id de centro costo para asociar con bodegas

--- database/seeders/StoreSeeder.php
+++ database/seeders/StoreSeeder.php
@@ -14,58 +14,72 @@ class StoreSeeder extends Seeder
     public function run(): void
     {
         Store::create([
+            'centrocosto_id' => 1,
             'name' => 'PRODUCTO TERMINADO',
             'description' => 'PRODUCTO QUE FABRICAMOS',            
         ]);
         Store::create([
+            'centrocosto_id' => 2,
             'name' => 'AVERIAS',
             'description' => 'PRODUCTO QUE YA NO SE PUEDEN COMERCIALIZAR',         
         ]);
         Store::create([
+            'centrocosto_id' => 2,
             'name' => 'LA AREPA',          
             'description' => 'BODEGA PARA DESPACHO DE SOLO ESE CLIENTE',         
         ]);
         Store::create([
+            'centrocosto_id' => 2,
             'name' => 'MAQUILA',          
             'description' => 'CARGUE DEL SERVICIO MAQUILA',         
         ]);
         Store::create([
+            'centrocosto_id' => 3,
             'name' => 'MEGACENTRAL',          
             'description' => 'Para traslado entre Bodegas',         
         ]);
         Store::create([
+            'centrocosto_id' => 7,
             'name' => 'GUADALUPE',          
             'description' => 'Para traslado entre Bodegas',         
         ]);
         Store::create([
+            'centrocosto_id' => 8,
             'name' => 'GALAN',          
             'description' => 'Para traslado entre Bodegas',         
         ]);
         Store::create([
+            'centrocosto_id' => 9,
             'name' => 'PALOQUEMADO',          
             'description' => 'Para traslado entre Bodegas',         
         ]);
         Store::create([
+           'centrocosto_id' => 10,
             'name' => 'SUBAZAR',          
             'description' => 'Para traslado entre Bodegas',         
         ]);
         Store::create([
+            'centrocosto_id' => 11,
             'name' => 'CERDO CENTRAL',          
             'description' => 'Para traslado entre Bodegas',         
         ]);
         Store::create([
+            'centrocosto_id' => 12,
             'name' => 'LECHONERIA',          
             'description' => 'Para traslado entre Bodegas',         
         ]);
         Store::create([
+            'centrocosto_id' => 14,
             'name' => 'LOCAL SOACHA',          
             'description' => 'Para traslado entre Bodegas',         
         ]);
         Store::create([
+            'centrocosto_id' => 1,
             'name' => 'PRODUCTO A GRANEL',          
             'description' => 'Cliente especiales',         
         ]);
         Store::create([
+            'centrocosto_id' => 3,
             'name' => 'CARVAJAL',          
             'description' => 'Para traslado entre Bodegas',         
         ]);
