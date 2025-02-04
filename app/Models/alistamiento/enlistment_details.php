9b13338f25b7da0ccafb0855782aefa54b7f73fe
eddieluis37 <eddierada@gmail.com>
2025-02-03 21:16:08 -0500

    agrega campos al modelo detalles de alistamiento

--- app/Models/alistamiento/enlistment_details.php
+++ app/Models/alistamiento/enlistment_details.php
@@ -9,5 +9,5 @@ class enlistment_details extends Model
 {
     use HasFactory;
     protected $table = 'enlistment_details';
-	protected $fillable = ['enlistments_id','products_id','meatcut_id','kgrequeridos','newstock','status'];
+	protected $fillable = ['enlistments_id','products_id','meatcut_id','kgrequeridos', 'porc_venta', 'costo_total', 'costo_kilo', 'utilidad', 'porc_utilidad', 'newstock','status'];
 }
