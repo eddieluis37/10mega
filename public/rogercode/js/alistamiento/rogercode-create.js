4a357a3b2f53b0fab093d9e6728d06629ba6bb67
eddieluis37 <eddierada@gmail.com>
2025-02-07 10:22:37 -0500

    Solicita confirmación al Carga al inventario

--- public/rogercode/js/alistamiento/rogercode-create.js
+++ public/rogercode/js/alistamiento/rogercode-create.js
@@ -59,46 +59,6 @@ sendData("/getproductos", dataform, token).then((result) => {
         selectProducto.appendChild(optionElement);
     });
 });
-/*$('.select2Prod').on('change', function() {
-    const selectedValue = $(this).val();
-    console.log("Selected value: " + selectedValue);
-    const selectedOption = $(this).find('option:selected');
-    const attributeStock = selectedOption.attr('data-stock');
-    console.log("Attribute value: " + attributeStock);
-    stockPadre.value = attributeStock;
-    const dataform = new FormData();
-    dataform.append("categoriaId", Number(selectedValue));
-    sendData("/getproductos",dataform,token).then((result) => {
-        console.log(result);
-        let prod = result.products;
-        console.log(prod);
-        selectProducto.innerHTML = "";
-        selectProducto.innerHTML += `<option value="">Seleccione el producto</option>`;
-        prod.forEach(option => {
-        const optionElement = document.createElement("option");
-        optionElement.value = option.id;
-        optionElement.text = option.name;
-        selectProducto.appendChild(optionElement);
-        });
-    });
-});*/
-
-/* btnAddAlist.addEventListener("click", (e) => {
-    e.preventDefault();
-    const dataform = new FormData(formDetail);
-    dataform.append("stockPadre", stockPadre.value);
-    sendData("/alistamientosavedetail", dataform, token).then((result) => {
-        console.log(result);
-        if (result.status === 1) {
-            $("#producto").val("").trigger("change");
-            formDetail.reset();
-            showData(result);
-        }
-        if (result.status === 0) {
-            errorMessage("Tienes campos vacios");
-        }
-    });
-}); */
 
 btnAddAlist.addEventListener("click", async (e) => {
     e.preventDefault();
@@ -272,136 +232,61 @@ tbodyTable.addEventListener("click", (e) => {
     }
 });
 
-tfootTable.addEventListener("click", (e) => {
+tfootTable.addEventListener("click", async (e) => {
     e.preventDefault();
     let element = e.target;
-    console.log(element);
+
     if (element.id === "addShopping") {
-        //added to inventory
-        console.log("click");
-        loadingStart(element);
-        const dataform = new FormData();
-        dataform.append("alistamientoId", Number(alistamientoId.value));
-        dataform.append("newStockPadre", Number(newStockPadre.value));
-     
-        dataform.append("stockPadre", Number(stockPadre.value));
-        dataform.append("productoPadre", Number(productoPadre.value));
-        dataform.append("storeId", Number(storeId.value));
-        dataform.append("storeId", Number(storeId.value));
-        sendData("/alistamientoAddShoping", dataform, token).then((result) => {
-            console.log(result);
-            if (result.status == 1) {
-                loadingEnd(element, "success", "Cargar al inventario");
-                element.disabled = true;
-                window.location.href = `/alistamiento`;
-            }
-            if (result.status == 0) {
-                loadingEnd(element, "success", "Cargar al inventario");
-                errorMessage(result.message);
-            }
-        });
-    }
-});
+        console.log("Se hizo clic en el botón addShopping");
 
-//if (addShopping) {
-/*addShopping.addEventListener('click', (e) => {
-        e.preventDefault();
-        const dataform = new FormData();
-        loadingStart(addShopping)
-        dataform.append("alistamientoId", Number(alistamientoId.value));
-        dataform.append("newStockPadre", Number(newStockPadre.value));
-        dataform.append("pesokg", Number(pesokg.value));
-        dataform.append("stockPadre", Number(stockPadre.value));
+        try {
+            const result = await showConfirmationAlert();
 
-        sendData("/alistamientoAddShoping",dataform,token).then((result) => {
-            console.log(result);
-            loadingEnd(addShopping,"success","Cargar al inventario")
-            //showData(result)
-        })
-    });*/
-//}
+            console.log("Resultado de la alerta:", result);
 
-/*selectCategoria.addEventListener("change", function() {
-    const selectedValue = this.value;
-    console.log("Selected value:", selectedValue);*/
-
-/*const dataform = new FormData();
-    dataform.append("categoriaId", Number(selectedValue));
-    sendData("/getproductos",dataform,token).then((result) => {
-        console.log(result);
-        let prod = result.products;
-        console.log(prod);
-        selectProducto.innerHTML = "";
-        selectProducto.innerHTML += `<option value="">Seleccione el producto</option>`;
-        prod.forEach(option => {
-        const optionElement = document.createElement("option");
-        optionElement.value = option.id;
-        optionElement.text = option.name;
-        selectProducto.appendChild(optionElement);
-        });
-    });*/
-
-//});
-
-/*tbodyTable.addEventListener("click", (e) => {
-    e.preventDefault(); 
-    let element = e.target;
-    if (element.name === 'btnDown') {
-        console.log(element);
-		swal({
-			title: 'CONFIRMAR',
-			text: '¿CONFIRMAS ELIMINAR EL REGISTRO?',
-			type: 'warning',
-			showCancelButton: true,
-			cancelButtonText: 'Cerrar',
-			cancelButtonColor: '#fff',
-			confirmButtonColor: '#3B3F5C',
-			confirmButtonText: 'Aceptar'
-		}).then(function(result) {
-			if (result.value) {
-                let id = element.getAttribute('data-id');
-                console.log(id);
+            if (result.value) { 
+                console.log("Usuario confirmó la acción, procediendo...");
+              
+                loadingStart(element);
+                
                 const dataform = new FormData();
-                dataform.append("id", Number(id));
-                dataform.append("compensadoId", Number(compensado_id.value));
-                sendData("/compensadodown",dataform,token).then((result) => {
-                    console.log(result);
-                    showData(result)
-                })
+                dataform.append("alistamientoId", Number(alistamientoId.value));
+                dataform.append("newStockPadre", Number(newStockPadre.value));
+                dataform.append("stockPadre", Number(stockPadre.value));
+                dataform.append("productoPadre", Number(productoPadre.value));
+                dataform.append("storeId", Number(storeId.value));
 
-			}
+                const response = await sendData("/alistamientoAddShoping", dataform, token);
+                console.log("Resultado de la petición:", response);
+                
+                if (response.status === 1) {
+                    loadingEnd(element, "success", "Cargar al inventario");
+                    element.disabled = true;
+                    window.location.href = `/alistamiento`;
+                } else {
+                    loadingEnd(element, "error", "Cargar al inventario");
+                    errorMessage(response.message);
+                }
 
-		})
-    }
-
-    if (element.name === 'btnEdit') {
-        console.log(element);
-        let id = element.getAttribute('data-id');
-        console.log(id);
-        const dataform = new FormData();
-        dataform.append("id", Number(id));
-        sendData("/compensadogetById",dataform,token).then((result) => {
-            console.log(result);
-            let editReg = result.reg;
-            console.log(editReg);
-            regDetail.value = editReg.id;
-            pcompra.value = formatCantidadSinCero(editReg.pcompra);
-            pesokg.value = formatCantidad(editReg.peso);
-            $('.select2Prod').val(editReg.products_id).trigger('change');
-        })
+            } else {
+                console.log("El usuario canceló la acción.");
+            }
+        } catch (error) {
+            console.error("Error en la ejecución:", error);
+        }
     }
 });
 
-
-
-pcompra.addEventListener("change", function() {
-  const enteredValue = formatMoneyNumber(pcompra.value);
-  console.log("Entered value: " + enteredValue);
-  pcompra.value = formatCantidadSinCero(enteredValue);
-});
-
-pesokg.addEventListener("change", function() {
-  const enteredValue = formatkg(pesokg.value);
-  console.log("Entered value: " + enteredValue);
-  pesokg.value = enteredValue;
-});*/
+function showConfirmationAlert() {
+    return Swal.fire({
+        title: "CONFIRMAR",
+        text: "¿Estás seguro de que deseas cargar el inventario?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Aceptar",
+        cancelButtonText: "Cancelar",
+    }).then((result) => {
+        console.log("Respuesta de SweetAlert2:", result);
+        return result;
+    });
+}
