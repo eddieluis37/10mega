3ef7fc3e6c0ef995a15e3bd363557019f3f98db2
eddieluis37 <eddierada@gmail.com>
2025-02-19 19:41:05 -0500

    Agrega menu y submenues ventas autoservicio , bar domicilio , parrilla

--- resources/views/layouts/theme/sidebar.blade.php
+++ resources/views/layouts/theme/sidebar.blade.php
@@ -126,8 +126,8 @@
             @endcan
 
             @can('ver_ventas')
-            <li class="">
-                <a href="{{ url('sales') }}" class="menu-toggle" data-active="false">
+            <li class="menu">
+                <a href="#sales" data-active="false" class="menu-toggle">
                     <div class="base-menu">
                         <div class="base-icons">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart">
@@ -139,7 +139,11 @@
                         <span>Ventas</span>
                     </div>
                 </a>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left">
+                    <polyline points="15 18 9 12 15 6"></polyline>
+                </svg>
             </li>
+
             @endcan
 
             @can('Pos_Create')
@@ -302,6 +306,64 @@
 
     <div id="compact_submenuSidebar" class="submenu-sidebar">
 
+        <div class="submenu" id="sales">
+            <ul class="submenu-list" data-parent-element="#sales">
+
+                <li>
+                    <a href="{{ url('sales') }}" style="display: flex; align-items: center">
+                        <!-- Ícono de Autoservicio -->
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart">
+                            <circle cx="9" cy="21" r="1"></circle>
+                            <circle cx="20" cy="21" r="1"></circle>
+                            <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+                        </svg>
+                        Autoservicio
+                    </a>
+                </li>
+
+                <li>
+                    <a href="{{ url('bar') }}" style="display: flex; align-items: center">
+                        <!-- Ícono de Bar -->
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-coffee">
+                            <path d="M18 8h1a4 4 0 0 1 0 8h-1"></path>
+                            <path d="M2 8h16v8a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V8z"></path>
+                            <line x1="6" y1="1" x2="6" y2="4"></line>
+                            <line x1="10" y1="1" x2="10" y2="4"></line>
+                            <line x1="14" y1="1" x2="14" y2="4"></line>
+                        </svg>
+                        Bar
+                    </a>
+                </li>
+
+                <li>
+                    <a href="{{ url('sales') }}" style="display: flex; align-items: center">
+                        <!-- Ícono de Domicilio -->
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-truck">
+                            <rect x="1" y="3" width="15" height="13"></rect>
+                            <polygon points="16 8 20 8 23 11 23 16 16 16"></polygon>
+                            <circle cx="5.5" cy="18.5" r="2.5"></circle>
+                            <circle cx="18.5" cy="18.5" r="2.5"></circle>
+                        </svg>
+                        Domicilio
+                    </a>
+                </li>
+
+                <li>
+                    <a href="{{ url('parrilla') }}" style="display: flex; align-items: center">
+                        <!-- Ícono de Parrilla -->
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bar-chart">
+                            <line x1="12" y1="20" x2="12" y2="10"></line>
+                            <line x1="18" y1="20" x2="18" y2="4"></line>
+                            <line x1="6" y1="20" x2="6" y2="16"></line>
+                        </svg>
+                        Parrilla
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+
+
         <div class="submenu" id="dashboard">
             <ul class="submenu-list" data-parent-element="#dashboard">
                 <li class="active">
@@ -412,7 +474,6 @@
 
         <div class="submenu" id="app">
             <ul class="submenu-list" data-parent-element="#app">
-
                 <li>
                     <a href="{{ url('inventario/cierre') }}" style="display: flex; align-items: center">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
@@ -425,8 +486,6 @@
                         Cierre de Inv KG
                     </a>
                 </li>
-
-
                 <li>
                     <a href="{{ url('inventory/utilidad') }}" style="display: flex; align-items: center">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-square">
