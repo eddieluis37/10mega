3c17ff124eb87ff4fd8691270e6b903c1274eb2d
eddieluis37 <eddierada@gmail.com>
2025-04-03 21:39:25 -0500

    ajuste nombre archivo

--- database/seeders/LoteSeeder.php
+++ database/seeders/LoteSeeder.php
@@ -14,7 +14,7 @@ class LoteSeeder extends Seeder
 
     public function run()
     {
-        $file = database_path('data/lotes4.csv');
+        $file = database_path('data/lote4.csv');
         $data = [];
 
         if (($handle = fopen($file, 'r')) !== false) {
